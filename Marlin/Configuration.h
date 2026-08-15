a14e9fdec6222aa5d206aaa5bcbe3f5d887282f2
Marijn <dutchhookbill@pm.me>
2026-08-15 08:53:24 +0200

    probe z offset update

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1731,7 +1731,7 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET { -43.18, 0, -2.8 } // (mm) X, Y, Z distance from Nozzle tip to Probe trigger-point
+#define NOZZLE_TO_PROBE_OFFSET { -43.18, 0, -3.04 } // (mm) X, Y, Z distance from Nozzle tip to Probe trigger-point
 
 // Enable and set to use a specific tool for probing. Disable to allow any tool.
 #define PROBING_TOOL 0
